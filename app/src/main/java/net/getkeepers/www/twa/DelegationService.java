02735c75e6efa108dd4d86374f35b93fbd5e752f
jaume-serra <jaumeserraa@gmail.com>
2022-07-04 12:33:02 +0200

    Init repositroy app

---
+++ app/src/main/java/net/getkeepers/www/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package net.getkeepers.www.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
